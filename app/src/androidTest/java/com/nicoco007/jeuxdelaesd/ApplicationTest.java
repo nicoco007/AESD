134e53eae375e3abca85c2cb8d5574d3f721df1a
Nicolas Gnyra <support@nicoco007.com>
2017-05-14 22:27:22 -0400

    Update copyright

--- app/src/androidTest/java/com/nicoco007/jeuxdelaesd/ApplicationTest.java
+++ app/src/androidTest/java/com/nicoco007/jeuxdelaesd/ApplicationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016 Nicolas Gnyra
+ * Copyright 2016–2017 Nicolas Gnyra
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
